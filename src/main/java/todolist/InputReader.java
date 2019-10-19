684de4c6f0151faf2d3bae7db286ce2a6e7b08aa
jshortz <jessica.shortz@gmail.com>
2019-10-19 22:56:21 +0200

    Added some documentation to InputReader and CommandReader

--- src/main/java/todolist/InputReader.java
+++ src/main/java/todolist/InputReader.java
@@ -1,5 +1,13 @@
-package todolist;
+/**
+ * Class that creates a Scanner object with defined methods for reading the next String or next Int
+ * Not necessary, but makes working with the Scanner more easily understood syntactically
+ * Alternate constructor takes a filename as a parameter and reads from the file rather than System.in
+ * (Currently not used in program, may be deprecated)
+ * @author  Jessica Shortz, based on work by Michael Kolling and David J. Barnes
+ * @version 2019.10.19
+ */
 
+package todolist;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,10 +16,12 @@ public class InputReader {
 
     protected Scanner inputReader;
 
+    // Constructor for user input
     public InputReader() {
         inputReader = new Scanner(System.in);
     }
 
+    // Constructor for reading from a file
     public InputReader(String filename) throws FileNotFoundException {
         try {
             File fileToReadFrom = new File("" + filename + "");
@@ -22,10 +32,18 @@ public class InputReader {
 
     }
 
+    /**
+     * A method to use a Scanner to read Strings
+     * @return next String that is input or read from a file
+     */
     public String readString() {
         return inputReader.nextLine();
     }
 
+    /**
+     * A method to use a Scanner to read ints
+     * @return next int that is input or read from a file
+     */
     public int readInt() {
         return inputReader.nextInt();
     }
