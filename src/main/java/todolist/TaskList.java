08b81bb2b06c2bad723c07d3cc61531c82609f20
jshortz <jessica.shortz@gmail.com>
2019-10-20 00:22:07 +0200

    Added documentation to TaskList

--- src/main/java/todolist/TaskList.java
+++ src/main/java/todolist/TaskList.java
@@ -1,5 +1,11 @@
-package todolist;
+/**
+ * TaskList creates a container for the Tasks in the to do list
+ * TaskList also facilitates accessors and mutators of the list itself
+ * @author  Jessica Shortz
+ * @version 2019.10.19
+ */
 
+package todolist;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -14,6 +20,9 @@ public class TaskList {
         commandReader = new CommandReader();
     }
 
+    /**
+     * Alters each field of a particular Task if user chooses to edit that field
+     */
     public void editTask() {
         Task taskToEdit = getTaskByTitle(commandReader.getTaskToEditFromUser());
         InputReader inputReader = new InputReader();
@@ -37,17 +46,27 @@ public class TaskList {
         }
     }
 
+    /**
+     * Adds a generic Task to the list
+     */
     public void addTask() {
         Task taskToAdd = commandReader.getTaskToAddFromUser();
         taskList.add(taskToAdd);
     }
 
+    /**
+     * Displays the to-do list
+     */
     public void displayList() {
         for (Task task : taskList) {
             System.out.println(task);
         }
     }
 
+    /**
+     * Checks whether the to do list is empty
+     * @return boolean true if list is empty
+     */
     public boolean isListEmpty() {
         if (taskList.size() == 0) {
             return true;
@@ -56,6 +75,12 @@ public class TaskList {
         }
     }
 
+    /**
+     * Accesses a Task in the list by title
+     * Also checks whether the title is shared by another task and distinguishes between them
+     * @return Task as determined by user inputs
+     * @param titleToGet the title of the Task that the user wishes to access
+     */
     public Task getTaskByTitle(String titleToGet) {
 
         ArrayList<Task> tasksByTitle = new ArrayList<>();
@@ -92,11 +117,18 @@ public class TaskList {
         }
     }
 
+    /**
+     * Removes the selected Task from the list
+     */
     public void removeTask() {
         Task taskToRemove = getTaskByTitle(commandReader.getTaskToRemoveFromUser());
         taskList.removeIf((task -> task == taskToRemove));
     }
 
+    /**
+     * Saves the list to an external file (taskList.tmp)
+     */
+    // TODO: Rename method to reflect SAVE ONLY
     public void saveAndQuit() throws IOException {
         FileOutputStream fos = new FileOutputStream("taskList.tmp");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
@@ -104,6 +136,10 @@ public class TaskList {
         oos.close();
     }
 
+    /**
+     * Sorts the to do list by date
+     * If user opts to sort by title, does a second sort by title
+     */
     public void sort() {
         taskList.sort(Comparator.comparing(Task::getDate));
         if (commandReader.getIfTitleSort()) {
@@ -111,6 +147,9 @@ public class TaskList {
         }
     }
 
+    /**
+     * If taskList.tmp exists, sets the to do list equal to its contents
+     */
     public void loadList() {
         File taskListFile = new File("taskList.tmp");
         if (taskListFile.exists()) {
