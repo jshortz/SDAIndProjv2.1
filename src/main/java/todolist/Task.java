5e0bb7747bee13fc407a5889275496b0746cbf0f
jshortz <jessica.shortz@gmail.com>
2019-10-19 23:36:45 +0200

    Updated Task documentation

--- src/main/java/todolist/Task.java
+++ src/main/java/todolist/Task.java
@@ -1,8 +1,17 @@
-package todolist;
+/**
+ * Class for building a Task object
+ * Each Task represents one entry on the user's to-do list
+ * Tasks may have the same title or same due date, but cannot have an identical due date and identical title
+ * @author  Jessica Shortz
+ * @version 2019.10.19
+ */
 
+package todolist;
 import java.io.Serializable;
 import java.util.GregorianCalendar;
 
+// Serializable used to output stream to file and read back again
+// serialVersionUID used to maintain file sync between versions of the compiled code
 public class Task implements Serializable {
 
     protected String title;
@@ -13,6 +22,13 @@ public class Task implements Serializable {
     protected boolean complete;
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Constructs a Task
+     * Title, project, date, and description are based on user input
+     * ID is generated from a combination of title and the date's hashCode
+     * field complete is set to false by default
+     * @return Task
+     */
     public Task(String title, String project, GregorianCalendar date, String description) {
         this.title = title;
         this.project = project;
@@ -22,6 +38,10 @@ public class Task implements Serializable {
         complete = false;
     }
 
+    /**
+     * Overwrites the parent toString method
+     * @return String with Task information
+     */
     public String toString() {
         String alwaysPrint = "\nTitle: " + title + "\nProject: " + project + "\nDue Date: " + date.getTime().toString() + "\nDescription: " + description + "\nID: " + id;
         if (complete == false) {
@@ -31,10 +51,18 @@ public class Task implements Serializable {
         }
     }
 
+    /**
+     * Returns the title of a Task
+     * @return String title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Returns the date of a Task
+     * @return GregorianCalendar date of a Task
+     */
     public GregorianCalendar getDate() {
         return date;
     }
