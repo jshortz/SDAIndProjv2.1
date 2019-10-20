f8729f0923d3871f5760b4d093ea19ed556d7a77
jshortz <jessica.shortz@gmail.com>
2019-10-20 17:56:21 +0200

    Fixed edit task to terminate or prompt for new title when no valid title.

--- src/main/java/todolist/TaskList.java
+++ src/main/java/todolist/TaskList.java
@@ -9,7 +9,6 @@ package todolist;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.GregorianCalendar;
 
 public class TaskList {
 
@@ -26,6 +25,14 @@ public class TaskList {
      */
     public void editTask() {
         Task taskToEdit = getTaskByTitle(commandReader.getTaskToEditFromUser());
+        if (taskToEdit == null) {
+            System.out.println("Your task list is empty or the title you supplied was invalid. Do you wish to enter a new title? Y or N?");
+            InputReader inputReader = new InputReader();
+            if (inputReader.readString().toUpperCase().equals("Y")) {
+                editTask();
+            }
+            return;
+        }
         InputReader inputReader = new InputReader();
         if (commandReader.editTitle()) {
             System.out.println("Please enter a new title:");
